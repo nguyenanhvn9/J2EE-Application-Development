d47284c514c3e651f85f0d96fbef5d0a2794e21e
HongThien1304 <thienpt2004@gmail.com>
2025-07-10 01:29:51 +0700

    Xóa tất cả file không liên quan

--- Tuan02/cos-141/src/main/java/com/hutech/cos141/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hutech.cos141.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
