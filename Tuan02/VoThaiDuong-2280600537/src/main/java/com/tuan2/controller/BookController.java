ed6b04f9809c3b06dc097d593f62599fbdfee7ec
Duong <duong18v04@gmail.com>
2025-07-10 10:52:10 +0700

    Tuan 02: Chinh sua ten project tuan 2

---
+++ Tuan02/VoThaiDuong-2280600537/src/main/java/com/tuan2/controller/BookController.java
@@ -0,0 +1,86 @@
+package com.tuan2.controller;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+import com.tuan2.model.Book;
+import com.tuan2.service.BookService;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/fetch-books")
+    public String fetchBooksFromApi() {
+
+        if (bookService.fetchBook()) {
+            return "Fetched and stored " + bookService.getAllBooks().size() + " books.";
+        }
+
+        return "Canot fetch api";
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Book>> getBooks(
+            @RequestParam(required = false) String author,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(bookService.getAllBooks(author, page, size));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public String addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+        return "Book added successfully!";
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Book>> updateBook(@PathVariable int id,
+            @RequestBody Book updatedBook) {
+        return ResponseEntity.ok(bookService.updateBook(id, updatedBook));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Optional<Book>> deleteBook(@PathVariable int id) {
+        return ResponseEntity.ok(bookService.deleteBook(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Book>> searchBook(@RequestParam("keyword") String keyword) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : bookService.getAllBooks()) {
+            if (book.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
+                    book.getAuthor().toLowerCase().contains(keyword.toLowerCase())) {
+                result.add(book);
+            }
+        }
+        return ResponseEntity.ok(result);
+    }
+
+}
--- Tuan02/baithuchanh4/src/main/java/com/tuan2/ExceptionHandling/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tuan2.ExceptionHandling;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
