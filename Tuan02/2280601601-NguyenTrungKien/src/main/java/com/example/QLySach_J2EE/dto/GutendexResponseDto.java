0c39aeb0682dc414bdabe09ed29efeafe63f10e0
TrungKevin <nguyentrungkien24sg@gmail.com>
2025-07-11 23:54:48 +0700

    Xóa thư mục Tuan02 khỏi Git để tạo lại

--- Tuan02/2280601601-NguyenTrungKien/src/main/java/com/example/QLySach_J2EE/dto/GutendexResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.QLySach_J2EE.dto;
-
-import lombok.Data;
-
-@Data
-public class GutendexResponseDto {
-    private GutendexBookDto[] results;
-} 
--- Tuan02/2280601601-NguyenTrungKien/src/main/java/com/example/QLySach_J2EE/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.QLySach_J2EE.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-} 
