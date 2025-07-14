fb263d4758045526f5a9cc67649b15a915aa70a5
had2810 <hoanganhduc42115@gmail.com>
2025-07-14 16:01:58 +0700

    tuan03 : 3 bai thuc hanh

---
+++ Tuan03/2280600715_HoangAnhDuc/buoi3book/src/main/java/com/hutech/buoi3book/dto/BookDTO.java
@@ -0,0 +1,39 @@
+package com.hutech.buoi3book.dto;
+
+public class BookDTO {
+    private long id;
+    private String title;
+    private String author;
+
+    public BookDTO() {}
+
+    public BookDTO(long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
