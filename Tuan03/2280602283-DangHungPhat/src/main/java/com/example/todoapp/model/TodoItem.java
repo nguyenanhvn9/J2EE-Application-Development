117f438ab27792b0572f2a2dbd20aca8351ce861
DangHungPhat <phat.dang638@gamil.com>
2025-07-15 23:13:28 +0700

    Hoan Thanh Tuan03

---
+++ Tuan03/2280602283-DangHungPhat/src/main/java/com/example/todoapp/model/TodoItem.java
@@ -0,0 +1,31 @@
+package com.example.todoapp.model;
+
+public class TodoItem {
+    private Long id;
+    private String title;
+    private boolean completed = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
